0ff4597132793ffb5918b69a9da24567e9ba70f3
DerMultiMedia <neal@diekloses.de>
2021-11-23 14:01:01 +0100

    Bubble Sort + Bubble Sort optimized

--- app/src/main/java/com/projektmanagement/gallapp/SecondFragment.java
+++ app/src/main/java/com/projektmanagement/gallapp/SecondFragment.java
@@ -20,6 +20,7 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class SecondFragment extends Fragment {
 
@@ -45,11 +46,79 @@ public class SecondFragment extends Fragment {
             @Override
             public void onClick(View view) {
 
-                LocalDate date = LocalDate.now();
+                binding.textView.setText("");
+                int[] Arr = new int[10000];
+                Random rand = new Random();
+                for(int i = 0; i < 10000; i++){
+                    Arr[i] = rand.nextInt(10000);
+                }
+                binding.textView.setText("Numbers generated");
+
+
+                //Normal Bubble Sort
+                final long startTime = System.currentTimeMillis();
+
+                for(int i = 0; i < Arr.length - 1; i++) {
+                    for(int j = 0; j < Arr.length - 1; j++) {
+                        // Comparing and swapping the elements
+                        if(Arr[j] > Arr[j+1]){
+                            int t = Arr[j];
+                            Arr[j] = Arr[j+1];
+                            Arr[j+1] = t;
+                        }
+                    }
+                }
+
+
+
+                binding.textView.setText("");
+//                for(int i = 0; i < Arr.length; i++) {
+//                    binding.textView.append(Integer.toString(Arr[i]) + ", ");
+//                }
+
+                final long endTime = System.currentTimeMillis();
+
+                System.out.println("Time: " + (endTime - startTime));
+
+
+                final long sTime = System.currentTimeMillis();
+                int n = Arr.length - 1;
+                boolean sorted = false;
+                int numOfIterations = 0;
+
+                while(!sorted) {
+                    sorted = true;
+                    for(int i = 0; i < n-numOfIterations; i++){
+                        if(Arr[i] > Arr[i+1]){
+                            int t = Arr[i];
+                            Arr[i] = Arr[i+1];
+                            Arr[i+1] = t;
+                            sorted = false;
+                            numOfIterations++;
+                        }
+                    }
+                }
+
+                binding.textView.setText("");
+//                for(int i = 0; i < Arr.length; i++) {
+//                    binding.textView.append(Integer.toString(Arr[i]) + ", ");
+//                }
+
+                final long eTime = System.currentTimeMillis();
+                System.out.println("Time: " + (eTime - sTime));
+
+                // binding.textView.setText(Arr.toString());
+
+                /*LocalDate date = LocalDate.now();
                 DayOfWeek Day = date.getDayOfWeek();
                 new ArrayList<>();
                 String[] Days = new String[]{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
                 binding.textView.setText("");
+
+                for(int i = 0; i<200000; i++){
+                    int a = 0;
+                    a += i;
+                }
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
@@ -66,7 +135,7 @@ public class SecondFragment extends Fragment {
                         break;
                     }
                 }
-
+*/
             }
         });
     }
