fbcdfb949e34f9be86732729fa3676641bdd2902
DerMultiMedia <neal@diekloses.de>
2021-11-25 16:22:09 +0100

    Weitere Änderungen.

--- app/src/main/java/com/projektmanagement/gallapp/SecondFragment.java
+++ app/src/main/java/com/projektmanagement/gallapp/SecondFragment.java
@@ -46,87 +46,103 @@ public class SecondFragment extends Fragment {
 
                 binding.textView.setText("");
                 binding.textView2.setText("");
-                int[] Arr = new int[1000];
+
+                int[] Arr = new int[20000];
                 Random rand = new Random();
-                for(int i = 0; i < 1000; i++){
-                    Arr[i] = rand.nextInt(10000);
+                for (int i = 0; i < 20000; i++) {
+                    Arr[i] = rand.nextInt(50000);
+                }
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
 
 
-                final long startTime = System.currentTimeMillis();
-
-                //Unoptimized Bubble Sort
-                for(int i = 0; i < Arr.length - 1; i++) {
-                    for(int j = 0; j < Arr.length - 1; j++) {
-                        // Vergleiche und swap die einträge wenn j > j+1
-                        if(Arr[j] > Arr[j+1]){
-                            int t = Arr[j];
-                            Arr[j] = Arr[j+1];
-                            Arr[j+1] = t;
-                        }
-                    }
-                }
-
-
-
-                binding.textView.setText("");
-                String ergebnis = "";
-                for(int i = 0; i < Arr.length; i++) {
-                    ergebnis += Arr[i] + ", ";
-
-                    binding.textView.setText(ergebnis);
-                }
-
-
-                final long endTime = System.currentTimeMillis();
-
-                System.out.println("Unoptimized Time: " + (endTime - startTime));
-                binding.Unoptimized.setText(Long.toString(endTime - startTime));
+                final long sTime = System.currentTimeMillis();
+                OBS(Arr, binding);
+                final long eTime = System.currentTimeMillis();
+                System.out.println("Optimized: " + (eTime - sTime));
+                binding.Optimized.setText(Long.toString(eTime - sTime));
 
                 try {
                     Thread.sleep(2000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                final long startTime = System.currentTimeMillis();
+                UBS(Arr, binding);
+                final long endTime = System.currentTimeMillis();
 
-                // Optimized Bubble Sort
-                final long sTime = System.currentTimeMillis();
-                int n = Arr.length - 1;
-                boolean sorted = false;
-                int zaehler = 0;
+                System.out.println("Unoptimized Time: " + (endTime - startTime));
+                binding.Unoptimized.setText(Long.toString(endTime - startTime));
 
-                while(!sorted) {
-                    sorted = true;
-                    for(int i = 0; i < n-zaehler; i++){
-                        if(Arr[i] > Arr[i+1]){
-                            int t = Arr[i];
-                            Arr[i] = Arr[i+1];
-                            Arr[i+1] = t;
-                            sorted = false;
-                            zaehler++;
-                        }
-                    }
+            }});
+    }
+
+    private void UBS(int[] Arr, FragmentSecondBinding binding) {
+
+        //Unoptimized Bubble Sort
+            for (int j = 0; j < Arr.length - 1; j++) {
+                // Vergleiche und swap die einträge wenn j > j+1
+                if (Arr[j] > Arr[j + 1]) {
+                    int t = Arr[j];
+                    Arr[j] = Arr[j + 1];
+                    Arr[j + 1] = t;
                 }
+        }
 
 
-                for (int j = 0; j < n; j++) {
-                    binding.textView2.append(Arr[j] + ", ");
-                }
+        binding.textView.setText("");
+        String ergebnis = "";
+        for (int i = 0; i < Arr.length; i++) {
+            ergebnis += Arr[i] + ", ";
 
-                final long eTime = System.currentTimeMillis();
-                System.out.println((eTime - sTime));
-                binding.Optimized.setText( Long.toString(eTime - sTime));
-
-
-
-            }
-        });
+        }
+        // binding.textView.setText(ergebnis);
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public static void OBS(int[] Arr, FragmentSecondBinding binding){
+        // Optimized Bubble Sort
+
+        StringBuilder s = new StringBuilder();
+        int n = Arr.length;
+
+        for (int i = 1; i < n - 1; i++) {
+            boolean sorted = true;
+            // skip the already sorted largest elements
+            for (int j = 0; j < n - 1 ; j++) {
+                if (Arr[j] > Arr[j + 1]) {
+                    int temp = Arr[j];
+                    Arr[j] = Arr[j + 1];
+                    Arr[j + 1] = temp;
+                    sorted = false;
+                }
+            }
+            if (sorted)
+                break;
+        }
+
+
+        for (int j = 0; j < n; j++) {
+            s.append(Arr[j] + ",");
+        }
+
+
+
+
+        // binding.textView2.setText(s);
+
+
+
+
     }
 
 }
